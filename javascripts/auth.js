98ccbe28314f040daf7cb004576cd2dd7842bf11
Alex Orga <alexorga.dev@gmail.com>
2018-06-13 18:50:46 -0500

    landing page styling basics established'

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -2,6 +2,13 @@ const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in.
+      $('#landing-page').removeClass('hide');
+      $('#authScreen').addClass('hide');
+      $('#messages-page').addClass('hide');
+      $('#tasks-page').addClass('hide');
+      $('#news-page').addClass('hide');
+      $('#friends-page').addClass('hide');
+      $('#events-page').addClass('hide');
       console.log('I am in!');
     } else {
       // No user is signed in.
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,11 @@
 const firebaseAPI = require('./firebaseAPI');
 const events = require('./events');
+const landingPage = require('./landingPage');
 
 const initializeApp = () => {
   firebaseAPI.getFirebseConfig();
   events.authEvents();
+  landingPage.domStringLandingPage();
 };
 
 initializeApp();
