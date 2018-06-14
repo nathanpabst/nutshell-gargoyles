fc3a4d82ad69f66627dda9912ff0487dbd76d940
Nathan Pabst <nathan.pabst@gmail.com>
2018-06-13 19:33:02 -0500

    added saveNewsArticle and saveNewsArticleEvent

---
+++ javascripts/newsEvents.js
@@ -0,0 +1,23 @@
+const {saveNewsArticle,} = require('./news');
+
+const saveNewsArticleEvent = () => {
+  $(document).on('click', '.addArticle', (e) => {
+    const articleToAddCard = $(e.target).closest('.article');
+    const articleToAdd = {
+      title: articleToAddCard.find('.article-title').text(),
+      synapsis: articleToAddCard.find('.article-synapsis').text(),
+      url: articleToAddCard.find('.article-url').text(),
+    };
+    saveNewsArticle.saveNewsArticleEvent(articleToAdd)
+      .then(() => {
+        alert('article saved');
+      })
+      .catch((error) => {
+        console.error('error in saving article', error);
+      });
+  });
+};
+
+module.exports = {
+  saveNewsArticleEvent,
+};
