6070d6bc7c6c1d1d41fb2b67b77ecc63e6302980
Xuehui Chen <jeffreychen2016@gmail.com>
2018-06-17 11:55:31 -0500

    added css style to edit and detele buttons in the message btn group

--- javascripts/messages/messagesEvents.js
+++ javascripts/messages/messagesEvents.js
@@ -75,7 +75,7 @@ let messageContent = '';
 const getMessageForEditEvent = () => {
   $(document).on('click','.chat-message-edit-btn', (e) => {
     messageId = $(e.target).closest('li').data('messageId');
-    messageContent = $(e.target).closest('.chat-message-wrapper-other').find('.chat-message-detail').html();
+    messageContent = $(e.target).closest('.chat-message-wrapper').find('.chat-message-detail').html();
     console.log(messageContent);
     $('#chat-message-content-edit').val(messageContent);
   });
